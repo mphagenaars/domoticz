a2f2ab53718bbb99994c770e1cf8cb6c68fbbb07
mphagenaars <mphagenaars@gmail.com>
2018-10-14 20:09:04 +0200

    Create script_device_container.lua
    domoticz device script

---
+++ script_device_container.lua
@@ -0,0 +1,115 @@
+-- script_device_container.lua
+
+-- DOMOTICZ
+-- device based script container
+
+-- VARIABELEN DEFINIEREN
+local time = os.date("*t")
+
+-- FUNCTIES DEFINIEREN
+-- index
+-- 1) verlichting: handmatige bediening met xiaomi-buttons
+-- 2) beweging: dummy motionsensor(s) aan zetten
+-- 3) verlichting: licht uit wanneer er geen beweging is in de woonkamer
+-- 4) verlichting: status "donker" bij minder dan 750 lux op xiaomi gateway
+-- 5) verlichting: licht aan in de hal wanneer er beweging is
+
+
+
+-- 1) handmatig licht aan/uit met Xiaomi schakelaars op de slaapkamer
+function lichtXiaomi(xiaomiSwitch)
+  if otherdevices[xiaomiSwitch] == "Click" then 
+    commandArray[#commandArray + 1] = {["lichtWoonkamer"] = "On"}
+    commandArray[#commandArray + 1] = {[xiaomiSwitch] = "Off AFTER 10"}
+  elseif otherdevices[xiaomiSwitch] == "Double Click" then 
+    commandArray[#commandArray + 1] = {["lichtWoonkamer"] = "Off"}
+    commandArray[#commandArray + 1] = {[xiaomiSwitch] = "Off AFTER 10"}
+  elseif otherdevices[xiaomiSwitch] == "Long Click" then 
+    commandArray[#commandArray + 1] = {["lichtWoonkamer"] = "Off"}
+    commandArray[#commandArray + 1] = {[xiaomiSwitch] = "Off AFTER 10"}
+  elseif otherdevices[xiaomiSwitch] == "Long Click Release" then 
+    commandArray[#commandArray + 1] = {["lichtWoonkamer"] = "Off"}
+    commandArray[#commandArray + 1] = {[xiaomiSwitch] = "Off AFTER 10"}
+  end
+end
+
+
+-- 2) beweging: dummy motionsensor(s) aan zetten
+function beweging(pir,sensor)
+  if otherdevices[pir] == "On" then
+    commandArray[sensor] = "On"
+  end
+end
+
+-- 3) verlichting: licht uit wanneer er geen beweging is in de woonkamer
+function lichtUit(lichtknop)
+  if otherdevices["bewegingZithoek"] == "Off" then
+    commandArray[lichtknop] = "Off"
+  end
+end
+
+
+-- 5) verlichting: licht aan in de hal wanneer er beweging is
+function lichtHal(lamp)
+  if otherdevices["pirHal"] == "On" then
+    commandArray[#commandArray + 1] = {[lamp] = "Set Level 55"}
+    commandArray[#commandArray + 1] = {[lamp] = "Off AFTER 450"}
+  end
+end
+
+-- test
+function test(device,lamp)
+  if otherdevices[device] == "flip90" then
+    commandArray[#commandArray + 1] = {[lamp] = "Set Level 57"}
+    commandArray[#commandArray + 1] = {[device] = "Off AFTER 5"}
+  elseif otherdevices[device] == "flip180" then
+    commandArray[#commandArray + 1] = {[lamp] = "Set Level 80"}
+    commandArray[#commandArray + 1] = {[device] = "Off AFTER 5"}
+  elseif otherdevices[device] == "tap_twice" then
+    commandArray[#commandArray + 1] = {[lamp] = "Set Level 25"}
+    commandArray[#commandArray + 1] = {[device] = "Off AFTER 5"}
+  elseif otherdevices[device] == "shake_air" then
+    commandArray[#commandArray + 1] = {[lamp] = "Off"}
+    commandArray[#commandArray + 1] = {[device] = "Off AFTER 5"}
+  end
+end
+
+
+
+-- vanaf hier regelen dat alles wordt geschakeld zoals gedefinieerd
+commandArray = {}
+-- xiaomi knoppen ivm bedienen van het licht
+if devicechanged["xiaomiSwitchJasmijn"] then 
+  local functie = lichtXiaomi("xiaomiSwitchJasmijn")
+end  
+if devicechanged["xiaomiSwitchMatthijs"] then 
+  local functie = lichtXiaomi("xiaomiSwitchMatthijs")
+end
+-- beweging zithoek
+if devicechanged["pirZithoek"] then
+  local functie = beweging("pirZithoek", "bewegingWoonkamer")
+end
+-- beweging eethoek
+if devicechanged["pirEethoek"] then
+  local functie = beweging("pirEethoek", "bewegingWoonkamer")
+end
+-- verlichting Zithoek
+if devicechanged["bewegingWoonkamer"] then 
+  local functie = lichtUit("lichtWoonkamer")
+end
+-- verlichting Hal
+if devicechanged["pirHal"] then  
+  local functie = lichtHal("lichtHal")
+end
+
+
+-- test
+if devicechanged["switchCube"] then 
+  local functie = test("switchCube", "lichtWoonkamer")
+end
+
+
+return commandArray
+
+
+
