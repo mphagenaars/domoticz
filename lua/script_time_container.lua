5422246c5eb05cdd174b63dd21cc529444a323b3
mphagenaars <matthijs@ThinkpadYoga.localdomain>
2019-01-24 10:09:10 +0100

    removed dusk sensor based on invalid LUX values reported by Xiaomi pirs

--- lua/script_time_container.lua
+++ lua/script_time_container.lua
@@ -20,27 +20,12 @@ if otherdevices["bewegingWoonkamer"] == "On" and no_motion_minutes > 30 then
    commandArray["bewegingWoonkamer"] = "Off"
 end
 
-
--- 2) verlichting: dusksensor aan/uit schakelen afhankelijk van lux in woonkamer
-timerDonker = tonumber(uservariables["dusksensorCounterDark"])
-timerLicht = tonumber(uservariables["dusksensorCounterLight"])
-if (tonumber(otherdevices["luxZithoek"]) < 100) or (tonumber(otherdevices["luxGateway"]) < 1000) then  
-   timerDonker = timerDonker + 1
-   timerLicht = 0
-else 
-   timerDonker = 0
-   timerLicht = timerLicht + 1
+-- 2. dummy device voor bepalen dag/nacht
+if timeofday["Nighttime"] == true and otherdevices["nightTime"] == "Off" then 
+   commandArray["nightTime"] = "On"
+elseif timeofday["Nighttime"] == false and otherdevices["nightTime"] == "On" then
+   commandArray["nightTime"] = "Off"  
 end
-
-commandArray["Variable:dusksensorCounterDark"] = tostring(timerDonker)
-commandArray["Variable:dusksensorCounterLight"] = tostring(timerLicht)
-
-if timerDonker > 4 and otherdevices['luxDonker'] == "Off" then 
-   commandArray["luxDonker"] = "On"
-elseif timerLicht > 4 and otherdevices["luxDonker"] == "On" then 
-   commandArray["luxDonker"] = "Off"
-end
-
 
 -- 3. bewegingsdetectie Vliering
 no_motion_minutes = tonumber(uservariables["nomotionCounterVliering"])
@@ -57,16 +42,7 @@ if otherdevices["bewegingVliering"] == "On" and no_motion_minutes > 10 then
    commandArray["bewegingVliering"] = "Off"
 end
 
-
--- 4. dummy device voor bepalen dag/nacht
-if timeofday["Nighttime"] == true and otherdevices["nightTime"] == "Off" then 
-   commandArray["nightTime"] = "On"
-elseif timeofday["Nighttime"] == false and otherdevices["nightTime"] == "On" then
-   commandArray["nightTime"] = "Off"  
-end
-
-
--- 5. bewegingsdetectie overloop
+-- 4. bewegingsdetectie overloop
 no_motion_minutes = tonumber(uservariables["nomotionCounterOverloop"])
  
 if (otherdevices["pirOverloop1"] == "Off" and otherdevices["pirOverloop2"] == "Off") then
