4c1ee471b89ae84d12de58e342f118905eec7921
mphagenaars <matthijs@ThinkpadYoga.localdomain>
2019-01-24 10:01:16 +0100

    conditional updating of setpoint (no longer every minute)

--- python/testToon.py
+++ python/testToon.py
@@ -17,18 +17,24 @@ toonGas = 104
 # debug?
 debug = 0
 
-# get thermostat info from Toon
-toon_url = 'http://%s/happ_thermstat?action=getThermostatInfo' % toon_ip
-response = requests.get(toon_url)
-thermostatInfo = json.loads(response.text)
-
-# get thermostat info from Domoticz to match toonstate & selector switch settings
+# check Domoticz to match toonstate & selector switch settings
 domo_url = 'http://%s/json.htm?type=devices&rid=108' % domoticz_ip
 response = requests.get(domo_url)
 checkDomo = json.loads(response.text)
 for x in checkDomo['result']:
-    level = x['Level']
+    levelD = x['Level']
 
+# check Domoticz to match actual setpoint in Toon & Domoticz
+domo_url = 'http://%s/json.htm?type=devices&rid=106' % domoticz_ip
+response = requests.get(domo_url)
+checkDomo = json.loads(response.text)
+for x in checkDomo['result']:
+    setPointD = x['Data']
+
+# get thermostat info from Toon
+toon_url = 'http://%s/happ_thermstat?action=getThermostatInfo' % toon_ip
+response = requests.get(toon_url)
+thermostatInfo = json.loads(response.text)
 
 # only proceed if Toon is online and ok
 if thermostatInfo['result'] == "ok":
@@ -58,7 +64,6 @@ if thermostatInfo['result'] == "ok":
     else: 
         cons = round(p1Info['dev_2.5']['profileInfo']['CurrentElectricityFlow'])
     gasUsage = round(p1Info['dev_2.1']['profileInfo']['CurrentGasQuantity'])
-    #print(str(p1Info))
 
     # updating Domoticz 
     # temp
@@ -66,13 +71,14 @@ if thermostatInfo['result'] == "ok":
     data = {'type':'command', 'param':'udevice','idx':'%s' % toonTemp,'nvalue':0,'svalue':temp} 
     resp = requests.get(url=domoticz_url, params=data)
     if debug == 1: print('update temp: %s' % json.loads(resp.text)['status'])
-    # setpoint
+    # setpoint (only update if device changed)
     data = {'type':'command', 'param':'udevice','idx':'%s' % toonSetpoint,'nvalue':0,'svalue':setpoint}
-    resp = requests.get(url=domoticz_url, params=data)
+    if round(float(setPointD)*100,0) != round(float(setpoint)*100,0):
+        resp = requests.get(url=domoticz_url, params=data)
     if debug == 1: print('update setpoint: %s' % json.loads(resp.text)['status'])
-    # selector / programma
+    # selector / programma (only update if device changed)
     data = {'type':'command', 'param':'switchlight', 'idx':'%s' % toonSelector, 'switchcmd':'Set Level', 'level':state}
-    if level != state:
+    if levelD != state:
         resp = requests.get(url=domoticz_url, params=data)
     if debug == 1: print('update programma: %s' % json.loads(resp.text)['status'])
     # p1: power
